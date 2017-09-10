e7b884137ae9d9f79cac631d542ad397f1495847
Deguang <better_li@live.com>
2017-09-11 07:33:34 +0800

    part1 init

---
+++ vue-part1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>part1 of learn vue</p>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: 'Hello world!' // 入门必备文案
+        }
+    }
+}
+</script>
+<style>
+    body {
+        background-color: #ccc;
+    }
+    #app {
+        border: 1px solid #000;
+    }
+</style>
+
